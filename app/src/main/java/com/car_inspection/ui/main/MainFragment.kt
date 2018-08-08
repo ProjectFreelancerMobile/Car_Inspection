c2fbf157b0dd07056411f31e2c4b3109f88499da
DESKTOP-SCE4HS6\CLICK <minhnguyencong92@gmail.com>
2018-08-08 10:41:30 +0700

    update ui

--- app/src/main/java/com/car_inspection/ui/main/MainFragment.kt
+++ app/src/main/java/com/car_inspection/ui/main/MainFragment.kt
@@ -14,16 +14,23 @@ import com.car_inspection.ui.adapter.StepAdapter
 import com.car_inspection.ui.base.BaseFragment
 import com.car_inspection.ui.record.RecordFragment
 import com.toan_itc.core.utils.addFragment
+import google.com.carinspection.DisposableImpl
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.main_fragment.*
+import java.util.concurrent.TimeUnit
 
 class MainFragment : BaseFragment(), StepAdapter.StepAdapterListener {
 
     private val SAVE_PATH: String = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).absolutePath
 
-    private var items: ArrayList<StepModifyModel>? = null
+    var items: ArrayList<StepModifyModel> = ArrayList()
     lateinit var stepAdapter: StepAdapter
     var isTakePicture = false
     var currentPosition = 0
+
+    var currentStep = 2
     companion object {
         fun newInstance() = MainFragment()
     }
@@ -42,29 +49,85 @@ class MainFragment : BaseFragment(), StepAdapter.StepAdapterListener {
         super.onViewCreated(view, savedInstanceState)
         rvSubStep.layoutManager = LinearLayoutManager(activity)
 
-        initTestData()
+        loadDataStep(currentStep)
+        updateProgressStep(currentStep)
 
-        stepAdapter = StepAdapter (this.activity!!)
-        stepAdapter.stepAdapterListener = this
-        stepAdapter.items = this.items
-        rvSubStep.adapter = stepAdapter
-        stepAdapter.notifyDataSetChanged()
+        btnSave.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                btnContinue.setActive(true)
 
-        pgStep.useRectangleShape()
-        pgStep.setMaximumPercentage(0.3f)
-        pgStep.gravity = Gravity.CENTER
-        pgStep.isShowingPercentage = true
-        pgStep.setTextColor(Color.BLACK)
-        pgStep.text = "30%"
+            }
+
+        })
+        btnContinue.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                Observable.just(1L).delay(100, TimeUnit.MILLISECONDS)
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(object : DisposableImpl<Long>() {
+                            override fun onNext(t: Long) {
+                                currentStep++
+                                loadDataStep(currentStep)
+                                btnContinue.setActive(false)
+                            }
+                        })
+            }
+        })
+
+        // disable scroll up android
+//        rvSubStep.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
+//            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+//            }
+//
+//            override fun onInterceptTouchEvent(rv: RecyclerView, event: MotionEvent): Boolean {
+//                val action = event.getAction()
+//                if (action == MotionEvent.ACTION_DOWN) {
+//                    lastY = event.getY()
+//                }
+//                 if (action == MotionEvent.ACTION_MOVE && event.getY() < lastY) {
+//                     return true
+//                }
+//                 else return false
+//            }
+//
+//            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+//            }
+//
+//        })
+    }
+    fun updateProgressStep(step: Int) {
+        val percent = step * 1f / 7
+        pgStep.setMaximumPercentage(percent)
+        pgStep.useRoundedRectangleShape(20.0f);
+        pgStep.setProgressColor(resources.getColor(R.color.blue_500))
+        pgStep.setProgressBackgroundColor(resources.getColor(R.color.blue_200))
+        pgStep.setText("${step * 100 / 7}%")
+        pgStep.setTextSize(14f)
+        pgStep.setTextColor(Color.WHITE)
+        pgStep.setGravity(Gravity.CENTER)
         pgStep.updateView()
     }
 
-    private fun convertStepOrinalModelsToStepModifyModels(stepOrinalModels: ArrayList<StepOrinalModel>): ArrayList<StepModifyModel> {
-        val stepModifyModels = ArrayList<StepModifyModel>()
-        if (stepOrinalModels.size > 0) {
+    fun loadDataStep(step: Int) {
+        if (layoutFinishCheck.visibility == View.VISIBLE)
+            layoutFinishCheck.visibility = View.GONE
+        items.clear()
+        items = initStepTestData(step)
+        stepAdapter = StepAdapter(this!!.activity!!)
+        stepAdapter.stepAdapterListener = this
+        stepAdapter.items = items
+        rvSubStep.adapter = stepAdapter
+
+        updateProgressStep(step)
+        tvStep.setText("Bước ${items?.get(0).subStep}: ${items?.get(0).subStepTitle1}")
+    }
+
+    fun convertStepOrinalModelsToStepModifyModels(stepOrinalModels: ArrayList<StepOrinalModel>): ArrayList<StepModifyModel> {
+        var stepModifyModels = ArrayList<StepModifyModel>()
+        if (stepOrinalModels != null && stepOrinalModels.size > 0) {
             for (stepOrinalModel in stepOrinalModels)
                 run {
-                    val stepModifyModel = StepModifyModel()
+                    var stepModifyModel = StepModifyModel()
                     stepModifyModel.subStep = stepOrinalModel.subStep
                     stepModifyModel.subStepTitle1 = stepOrinalModel.subStepTitle1
                     stepModifyModel.subStepTitle2 = stepOrinalModel.subStepTitle2
@@ -75,23 +138,31 @@ class MainFragment : BaseFragment(), StepAdapter.StepAdapterListener {
         return stepModifyModels
     }
 
-    private fun initTestData() {
-        val stepOrinalModels = ArrayList<StepOrinalModel>()
-        for (i in 1..20) {
-            val stepmodify = StepOrinalModel()
-            stepmodify.step = "2"
-            stepmodify.subStep = "2." + i
+    fun initStepTestData(step: Int): ArrayList<StepModifyModel> {
+        var stepOrinalModels = ArrayList<StepOrinalModel>()
+        var size = 5
+        if (step % 2 == 0)
+            size = 4;
+
+        for (i in 1..size) {
+            var stepmodify = StepOrinalModel()
+            stepmodify.step = "$step"
+            stepmodify.subStep = "$step." + i
             stepmodify.subStepTitle1 = "bên ngoài xe"
             stepmodify.subStepTitle2 = "bên trái trước"
             stepmodify.subStepTitle3 = "bên ngoài cửa xe"
             stepOrinalModels.add(stepmodify)
         }
 
-        items = convertStepOrinalModelsToStepModifyModels(stepOrinalModels)
+        return convertStepOrinalModelsToStepModifyModels(stepOrinalModels)
     }
 
-    private fun autoScrollAfterCheckComplete() {
-        rvSubStep.post { rvSubStep.smoothScrollBy(0,  stepAdapter.heightItem) }
+    fun autoScrollAfterCheckComplete() {
+        rvSubStep.post(object : Runnable {
+            override fun run() {
+                rvSubStep.smoothScrollBy(0, stepAdapter.heightItem)
+            }
+        })
     }
 
     override fun onRadioGroupCheckChangeListner(group: RadioGroup, checkId: Int, position: Int) {
@@ -110,21 +181,24 @@ class MainFragment : BaseFragment(), StepAdapter.StepAdapterListener {
                 items?.get(position)?.rating = "F"
             }
         }
-        if (stepAdapter.isFinishCheckItem()) {
-            layoutFinishCheck.visibility = View.VISIBLE
-        } else layoutFinishCheck.visibility = View.GONE
+        if (stepAdapter.isFinishCheckItem() && currentStep < 7) {
+            if (layoutFinishCheck.visibility == View.GONE)
+                layoutFinishCheck.visibility = View.VISIBLE
+        }
+        //else layoutFinishCheck.visibility = View.GONE
 
-        if (position < stepAdapter.itemCount - 3)
-            autoScrollAfterCheckComplete()
+//        if (position < stepAdapter.itemCount - 3)
+//            autoScrollAfterCheckComplete()
     }
 
-    private fun showLayoutTakepicture() {
+
+    fun showLayoutTakepicture() {
         isTakePicture = true
         layoutTakePicture.visibility = View.VISIBLE
         layoutVideo.visibility = View.GONE
     }
 
-    private fun showLayoutVideo() {
+    fun showLayoutVideo() {
         isTakePicture = false
         layoutTakePicture.visibility = View.GONE
         layoutVideo.visibility = View.VISIBLE
