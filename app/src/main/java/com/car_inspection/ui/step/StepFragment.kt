3701ed60e8b7a34839ca57fbe5ecb4124125348f
Van Toan <huynhvantoan.itc@gmail.com>
2018-08-31 11:39:58 +0700

    add libs ffmeg.

--- app/src/main/java/com/car_inspection/ui/step/StepFragment.kt
+++ app/src/main/java/com/car_inspection/ui/step/StepFragment.kt
@@ -411,4 +411,76 @@ class StepFragment : BaseDataFragment<StepViewModel>(), StepAdapter.StepAdapterL
 
         fun onConnected(connectedAccountName: String)
     }
+
+
+   /* if (FFmpeg.getInstance(this).isSupported())
+    {
+        // ffmpeg is supported
+        versionFFmpeg()
+        //ffmpegTestTaskQuit();
+    }
+    else
+    {
+        // ffmpeg is not supported
+        Timber.e("ffmpeg not supported!")
+    }
+
+    if (FFprobe.getInstance(this).isSupported())
+    {
+        // ffprobe is supported
+        versionFFprobe()
+    }
+    else
+    {
+        // ffprobe is not supported
+        Timber.e("ffprobe not supported!")
+    }
+
+    private void ffmpegTestTaskQuit() {
+        String[] command = {"-i", "input.mp4", "output.mov"};
+
+        final FFtask task = FFmpeg.getInstance(this).execute(command, new ExecuteBinaryResponseHandler() {
+            @Override
+            public void onStart() {
+                Timber.d( "on start");
+            }
+
+            @Override
+            public void onFinish() {
+                Timber.d("on finish");
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Timber.d("RESTART RENDERING");
+                        ffmpegTestTaskQuit();
+                    }
+                }, 5000);
+            }
+
+            @Override
+            public void onSuccess(String message) {
+                Timber.d(message);
+            }
+
+            @Override
+            public void onProgress(String message) {
+                Timber.d(message);
+            }
+
+            @Override
+            public void onFailure(String message) {
+                Timber.d(message);
+            }
+        });
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Timber.d( "STOPPING THE RENDERING!");
+                task.sendQuitSignal();
+            }
+        }, 8000);
+    }
+
+    */
 }
